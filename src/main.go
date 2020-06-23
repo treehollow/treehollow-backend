625a1e67381eef0ce80196aee0c77f9c4f9f8f1c
thuhole <thuhole@protonmail.com>
2020-06-23 23:00:53 +0800

    migration test

--- src/main.go
+++ src/main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/robfig/cron"
 	"github.com/spf13/viper"
-	"log"
 )
 
 func migrate() {
@@ -62,20 +59,20 @@ func main() {
 		migrate()
 	}
 
-	log.Println("start timestamp: ", getTimeStamp())
-	if false == viper.GetBool("is_debug") {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
-	var err error
-	hotPosts, _ = dbGetHotPosts()
-	c := cron.New()
-	_, _ = c.AddFunc("*/10 * * * *", func() {
-		hotPosts, err = dbGetHotPosts()
-		log.Println("refreshed hotPosts ,err=", err)
-	})
-	c.Start()
-
-	listenHttp()
+	//log.Println("start timestamp: ", getTimeStamp())
+	//if false == viper.GetBool("is_debug") {
+	//	gin.SetMode(gin.ReleaseMode)
+	//}
+	//
+	//var err error
+	//hotPosts, _ = dbGetHotPosts()
+	//c := cron.New()
+	//_, _ = c.AddFunc("*/10 * * * *", func() {
+	//	hotPosts, err = dbGetHotPosts()
+	//	log.Println("refreshed hotPosts ,err=", err)
+	//})
+	//c.Start()
+	//
+	//listenHttp()
 
 }
