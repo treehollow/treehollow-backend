270d7e84d5676eb033279d7f50978da1a2ba6bdc
thuhole <thuhole@protonmail.com>
2020-12-01 08:19:55 +0800

    v2.0.0-rc9

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -128,7 +128,7 @@ func SavePost(uid int32, text string, tag string, typ string, filePath string) (
 }
 
 func GetHotPosts() (posts []structs.Post, err error) {
-	err = db.Where("pid>(SELECT MAX(id)-2000 FROM posts)").
+	err = db.Where("id>(SELECT MAX(id)-2000 FROM posts)").
 		Order("like_num*3+reply_num+UNIX_TIMESTAMP(created_at)/1800-report_num*10 DESC").Limit(200).Find(&posts).Error
 	return
 }
